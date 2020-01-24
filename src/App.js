7daee6bb10477ea9b8fc43ca5d6357fed8577604
Mae Kangsadan <maemeier@live.com>
2020-01-24 08:11:58 +0100

    update state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>HI Mae</h1>
-      <SeasonDisplay />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  render() {
+    return (
+      <div>
+        <SeasonDisplay />
+        <h1>Latitude: {this.state.lat} </h1>{" "}
+        <h1>error: {this.state.errorMessage}</h1>
+      </div>
+    );
+  }
 }
+export default App;
