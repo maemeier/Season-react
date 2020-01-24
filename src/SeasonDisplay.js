5017f43598066b4aac76f838b17c293776b8933b
Mae Kangsadan <maemeier@live.com>
2020-01-24 09:15:15 +0100

    add seasonConfig

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,16 @@
 import React from "react";
 // check the season
 
+const seasonConfig = {
+  summer: {
+    text: "Let's go to the beach, and eat some peach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Baby, it's cold outside",
+    iconName: "snowflake"
+  }
+};
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -10,8 +20,16 @@ const getSeason = (lat, month) => {
 };
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
-  return <div />;
+  const { text, iconName } = seasonConfig[season];
+  console.log("text", text);
+  console.log("iconname", iconName);
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
